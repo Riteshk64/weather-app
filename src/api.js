b86b1647c839079cdccfef9413239e3461c9d966
Riteshk64 <riteshkumar.hb@gmail.com>
2024-02-23 23:29:07 +0530

    Completed search bar.

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const url = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "94eee76234mshe63769621956b16p1b748ajsn89f0bd1491c2",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { url, geoApiOptions } from "../../api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
@@ -9,12 +10,34 @@ const Search = ({ onSearchChange }) => {
     onSearchChange(searchData);
   };
 
+  const loadOptions = async (searchInputValue) => {
+    try {
+      const response = await fetch(
+        `${url}/cities?minPopulation=1000000&namePrefix=${searchInputValue}`,
+        geoApiOptions
+      );
+      const result = await response.json();
+
+      return {
+        options: result.data.map((city) => {
+          return {
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name}, ${city.countryCode}`,
+          };
+        }),
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <AsyncPaginate
       placeholder="Search for city"
       debounceTimeout={600}
       value={search}
       onChange={handleOnChange}
+      loadOptions={loadOptions}
     />
   );
 };
