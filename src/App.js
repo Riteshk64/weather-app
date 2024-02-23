bfcc24b3bc1b52ceeba60e136d8c635fba0e56f2
Riteshk64 <riteshkumar.hb@gmail.com>
2024-02-23 23:00:17 +0530

    AsyncPaginate to create autocomplete search bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import Search from "./components/search/Search";
 
 function App() {
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
-
-    console.log("Latitude is:", lat);
-    console.log("Longitude is:", long);
-  }, [lat, long]);
-
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+    </div>
+  );
 }
-
 export default App;
